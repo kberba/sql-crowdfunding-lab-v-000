0afac9836d93382892f5ae6b80866579e5fd2224
Karen Berba <kberba@gmail.com>
2018-02-16 03:46:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,13 +7,12 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
-  SELECT title, name FROM pledges
+" SELECT title, name FROM pledges
   LEFT JOIN projects
       ON pledges.project_id = projects.id
   LEFT JOIN users
       ON pledges.user_id = users.id
-  ORDER BY name ASC
+  ORDER BY name ASC "
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
