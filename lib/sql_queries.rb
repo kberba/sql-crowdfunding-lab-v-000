b88d6b40917355d593ea266ed980d19057daae1b
Karen Berba <kberba@gmail.com>
2018-02-16 03:46:05 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,11 +8,12 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
-  SELECT title
-  FROM projects
-  LEFT JOIN pledges
-    ON i
-  ORDER BY name
+  SELECT title, name FROM pledges
+  LEFT JOIN projects
+      ON pledges.project_id = projects.id
+  LEFT JOIN users
+      ON pledges.user_id = users.id
+  ORDER BY name ASC
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
