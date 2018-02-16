609f8e10dd0312943750d8db2e9707c9497c3686
Karen Berba <kberba@gmail.com>
2018-02-16 04:34:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,13 @@ SELECT users.name, users.age, SUM(pledges.amount) AS project_pledge_total FROM p
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"
+SELECT projects.title, SUM(pledges.amount) AS project_pledge_total FROM projects
+    INNER JOIN pledges ON projects.id = pledges.project_id
+    JOIN users ON pledges.user_id = users.id
+    WHERE 
+;
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
